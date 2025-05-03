9b355146be2a55a00a74878f8f6746415be93c83
Darshuu25 <vy82@njit.edu>
2025-05-03 16:27:01 -0400

    added depency to get minio client

--- app/utils/minio_utils.py
+++ app/utils/minio_utils.py
@@ -1,3 +1,4 @@
+# Adjust the import according to your project structure
 from settings.config import MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_BUCKET
 from minio import Minio
 
@@ -6,11 +7,10 @@ def get_minio_client():
         endpoint="minio:9000",
         access_key=MINIO_ACCESS_KEY,
         secret_key=MINIO_SECRET_KEY,
-        secure=False 
+        secure=False
     )
-    
+
     if not client.bucket_exists(MINIO_BUCKET):
         client.make_bucket(MINIO_BUCKET)
         
     return client
-
